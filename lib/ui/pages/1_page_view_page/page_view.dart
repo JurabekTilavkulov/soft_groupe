2f8cd48bb793efa6b28fdb2d693f7e6d46e2c981
JurabekTilavkulov <71831459+JurabekTilavkulov@users.noreply.github.com>
2023-12-26 14:25:32 +0500

    skeliti tuzildi

---
+++ lib/ui/pages/1_page_view_page/page_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PageView extends StatelessWidget {
+  const PageView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
